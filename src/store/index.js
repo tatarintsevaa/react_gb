b13f2fe0632a23bfed36b23d442be0d17c4b9b55
Tatarintsev Alexey <tatarincev.alexey@advcake.com>
2022-10-11 09:17:05 +0300

    lesson 7

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,24 @@
 import {configureStore, combineReducers} from "@reduxjs/toolkit";
 import chatsSlice from "./chatsSlice";
 import messageListSlice from "./messageListSlice";
+import {botAnswer} from "../asyncActions/botAnswer";
+import logger from "redux-logger";
+import storage from 'redux-persist/lib/storage';
+import {persistReducer} from "redux-persist";
 
 const rootReducer = combineReducers({
     chatList: chatsSlice,
     messageList: messageListSlice
 })
 
+const persistConfig = {
+    key: 'root',
+    storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-    reducer: rootReducer
+    reducer: persistedReducer,
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat([botAnswer, logger])
 })
